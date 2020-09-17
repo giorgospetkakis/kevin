d6c994d39225fade8b8e8e4c12acba5b54eef983
Giorgos Petkakis <giorgospetkakis@users.noreply.github.com>
2020-09-17 20:26:01 +0200

    Basic functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const SlackBot = require('slackbots');
 const axios = require('axios');
 
 const bot = new SlackBot({
-    token:"xoxb-1376262550244-1367010574181-8lufkOlQX3xxnzVrQp2L3cBS",
-    name:"kevin_the_office_dog"
+    token:"xoxb-1376262550244-1383262827025-g9rnoaeXthi6wvdAqWfCntz1",
+    name:"Kevin the Office Dog"
 });
 
 // Start handler
@@ -12,5 +12,49 @@ bot.on('start', () => {
         icon_emoji: ":dog:"
     };
 
-    bot.postMessageToChannel("general", "Woof!", params);
+    bot.postMessageToChannel("kevins-kennel", "Woof!", params);
 });
+
+// Error Handler
+bot.on('error', err => console.log(err));
+
+// Message Handler
+bot.on('message', data => {
+    if (data.type !== 'message') {
+      return;
+    }
+
+    const user = bot.getUsers()._value.members.find(element => element.id == data.user);
+
+    if(user)
+    {
+        handleMessage(data.text, user.name);
+    }
+    
+  });
+  
+  // Respond to Data
+  function handleMessage(message, user) {
+    if (message.includes("good boy"))
+    {
+        boy(user);
+
+    } else {
+        woof(user);
+    }
+    
+  }
+  
+  // I know the answer to this one!!!!
+  function boy(user) {
+
+    bot.postMessageToUser(user, "I am a good boy!!!!!!!!!!!"); 
+  
+  }
+
+    // Woof at user!!!!
+  function woof(user) {
+
+    bot.postMessageToUser(user, "Woof!"); 
+  
+  }
