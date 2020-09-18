012f28a257a81d39248173893be9d2f5159d58c9
Giorgos Petkakis <giorgospetkakis@users.noreply.github.com>
2020-09-18 09:42:57 +0200

    Reorganize and properly format the code

--- index.js
+++ index.js
@@ -1,105 +1,112 @@
-const SlackBot = require('slackbots');
-const Discord = require('discord.js');
-const RiveScript = require('rivescript');
-var express = require('express');
-var app     = express();
+const SlackBot = require("slackbots");
+const Discord = require("discord.js");
+const RiveScript = require("rivescript");
+var express = require("express");
 
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
+// Fix the Heroku $PORT error
+var app = express();
+app.set("port", process.env.PORT || 5000);
+app
+  .get("/", function (request, response) {
+    var result = "App is running";
     response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
-const slack_bot = new SlackBot({
-    token: process.env.SLACK_TOKEN,
-    name:"Kevin the Office Dog"
-});
-
-const discord_bot = new Discord.Client();
-const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-
-var rive = new RiveScript({utf8: true});
-
-discord_bot.login(DISCORD_TOKEN);
-
-// Start handler
-slack_bot.on('start', () => {
-    init();
-
-    slack_bot.postMessageToChannel("general", "I awake! :dog:");
-});
-
-// Start handler
-discord_bot.on('ready', () => {
-    init();
-});
-
-discord_bot.on('message', msg => {
-    if(msg.author.id === discord_bot.user.id){
-        return;
-    }
-    handleDiscordMessage(msg, replyToDiscordUser);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
   });
 
-function init()
-{
-    rive.loadFile([
-        "substitutions.rive",
-        "kevin.rive"
-        
-      ]).then(loading_done).catch(loading_error);
+// Initialize Rivescript
+var rive = new RiveScript({ utf8: true });
+rive
+  .loadFile(["substitutions.rive", "kevin.rive"])
+  .then(loading_done)
+  .catch(loading_error);
+
+// Rivescript loading done
+function loading_done() {
+  console.log("Rivescript bot has finished loading!");
+  rive.sortReplies();
 }
 
+// Rivescript loading failed
+function loading_error(error, filename, lineno) {
+  console.log("Error when loading files: " + error);
+}
+
+// ==== SLACK =====
+// Initialize SlackBot
+const slack_bot = new SlackBot({
+  token: process.env.SLACK_TOKEN,
+  name: "Kevin the Office Dog",
+});
+
+// Post (Re)start Message
+slack_bot.on("start", () => {
+  console.log("Slack bot has started.");
+});
+
 // Error Handler
-slack_bot.on('error', err => console.log(err));
+slack_bot.on("error", (err) => console.log(err));
 
 // Message Handler
-slack_bot.on('message', data => {
-    if (data.type !== 'message') {
-      return;
-    }
+slack_bot.on("message", (data) => {
+  if (data.type !== "message") {
+    return;
+  }
 
-    const user = slack_bot.getUsers()._value.members.find(element => element.id == data.user);
+  const user = slack_bot
+    .getUsers()
+    ._value.members.find((element) => element.id == data.user);
 
-    if(user) {
-        handleSlackMessage(data.text, user.name);
-    }
-    
+  if (user) {
+    handleSlackMessage(data.text, user.name);
+  }
+});
+
+// Query rive to respond to messages
+function handleSlackMessage(message, user) {
+  rive.reply(user, message).then(function (reply) {
+    replyToSlackUser(user, reply);
   });
-  
-  // Query rive to respond to messages
-  function handleSlackMessage(message, user){
-    rive.reply(user, message).then(function(reply) {
-        replyToSlackUser(user, reply);
-    });
-  }
+}
 
-    // Query rive to respond to messages
-    function handleDiscordMessage(message, user){
-        rive.reply(user, message.content).then(function(reply) {
-            replyToDiscordUser(message, reply);
-        });
-      }
+// Reply to the user
+function replyToSlackUser(user, reply) {
+  slack_bot.postMessageToUser(user, reply);
+}
 
-  function replyToSlackUser(user, reply)
-  {
-    slack_bot.postMessageToUser(user, reply);
-  }
+// ===== DISCORD =====
+const discord_bot = new Discord.Client();
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+discord_bot.login(DISCORD_TOKEN);
 
-  function replyToDiscordUser(message, reply)
-  {
-    message.channel.send(reply);
-  }
+// Post (re)start message
+discord_bot.on("ready", () => {
+  console.log("Discord bot has started.");
+});
 
-  function loading_done() {
-    console.log("Bot has finished loading!");
-    rive.sortReplies();
+// Error Handler
+discord_bot.on("error", (err) => console.log(err));
+
+// Message handler
+discord_bot.on("message", (msg) => {
+  if (msg.author.id === discord_bot.user.id) {
+    return;
   }
-  
-  function loading_error(error, filename, lineno) {
-    console.log("Error when loading files: " + error);
-  }
+  handleDiscordMessage(msg, replyToDiscordUser);
+});
+
+// Query rive to respond to messages
+function handleDiscordMessage(message, user) {
+  rive.reply(user, message.content).then(function (reply) {
+    replyToDiscordUser(message, reply);
+  });
+}
+
+// Reply to the user
+function replyToDiscordUser(message, reply) {
+  message.channel.send(reply);
+}
