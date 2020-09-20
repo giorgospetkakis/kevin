600b1bfb39763f7acb5195c266f205bb389c3a17
Giorgos Petkakis <giorgospetkakis@users.noreply.github.com>
2020-09-20 08:29:56 +0200

    Bring back the Heroku crash fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+var express = require("express");
 var dotenv = require("dotenv");
 var discord = require('./src/discord-bot');
 var slackbot = require("./src/slack-bot");
@@ -8,3 +8,18 @@ var slackbot = require("./src/slack-bot");
 // function report() {
 //   console.log("report"); //TODO: Make this an actual report for the logs
 // }
+
+// Fix the Heroku $PORT error
+var app = express();
+app.set("port", process.env.PORT || 5000);
+app
+  .get("/", function (request, response) {
+    var result = "App is running";
+    response.send(result);
+  })
+  .listen(app.get("port"), function () {
+    console.log(
+      "App is running, server is listening on port ",
+      app.get("port")
+    );
+  });
